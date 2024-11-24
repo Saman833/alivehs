9d1ff07f0d88be321332f64d194543b46e862ef6
Saman833 <s.ahmadifar2000@gmail.com>
2024-11-24 11:43:58 +0100

    Membership -mcfsr added

--- database/factories/MembershipFactory.php
+++ database/factories/MembershipFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Club>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Membership>
  */
-class ClubFactory extends Factory
+class MembershipFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,7 @@ class ClubFactory extends Factory
     public function definition(): array
     {
         return [
-
+            //
         ];
     }
 }
